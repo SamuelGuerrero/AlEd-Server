8f6e7f836061f85a2801cec2fcbae00d8c27adb0
Samuel Guerrero <itougn15@gmail.com>
2023-06-08 21:59:11 -0600

    More changes

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 //Middleware
-app.use(express.json());
+//app.use(express.json());
 app.use(
   cors({
     origin: "https://aled-server.onrender.com",
